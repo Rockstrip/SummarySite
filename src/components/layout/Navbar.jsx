d1b23ca7755e78ee6a80c79e69fa9b9ae63f54b5
Artur Kashuba <40093366+Rockstrip@users.noreply.github.com>
2025-05-15 20:56:04 -0400

    navbar button fix

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,13 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <header>
       <nav>
-        <Link to="/"><button className="active"><h1>About Me</h1></button></Link>
-        <Link to="/portfolio"><button><h1>Portfolio</h1></button></Link>
-        <Link to="/contact"><button><h1>Contact</h1></button></Link>
+        <Link to="/">
+          <button className={location.pathname === '/' ? 'active' : ''}>
+            <h1>About Me</h1>
+          </button>
+        </Link>
+        <Link to="/portfolio">
+          <button className={location.pathname === '/portfolio' ? 'active' : ''}>
+            <h1>Portfolio</h1>
+          </button>
+        </Link>
+        <Link to="/contact">
+          <button className={location.pathname === '/contact' ? 'active' : ''}>
+            <h1>Contact</h1>
+          </button>
+        </Link>
       </nav>
     </header>
   );
